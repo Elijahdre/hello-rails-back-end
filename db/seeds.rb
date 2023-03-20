69fbb2da4df3fc90022e0fa41d8436b648c6a86d
Elijah Odjegba <elijahodjegba@gmail.com>
2023-03-20 16:24:29 +0100

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create!(message: "welcome to Africa")
+Greeting.create!(message: "welcome to Uganda")
+Greeting.create!(message: "Hello Kigali")
+Greeting.create!(message: "Hi Kango")
+Greeting.create!(message: "Elijah")
